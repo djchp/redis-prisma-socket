a42188f23fb8038ef19e0b3f7e22c1b1c164f3ef
djchp <dawid.sliwka321@gmail.com>
2022-09-26 23:14:23 +0200

    .

--- packages/server/Config/config.js
+++ packages/server/Config/config.js
@@ -1,6 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const Redis = require('ioredis')
-const redisClient = new Redis()
+const redisClient = new Redis(process.env.REDIS_URL)
 
 module.exports = {redisClient, prisma} ;
